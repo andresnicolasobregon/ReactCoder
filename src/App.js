7c8f6de1818779b691a281ff83c6f484aed09381
Andres Obregon <andresnicolasobregon@gmail.com>
2021-12-17 14:32:57 -0300

    nuevos arreglos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import "./estilo.css"
 import NavBar from "./components/NavBar.js"
 
 function App(){
-    return <NavBar/>      
+    return (
+        <div>
+            <h1>E-commerce</h1>
+            <NavBar /> 
+        </div>
+    )     
 }
 export default App
