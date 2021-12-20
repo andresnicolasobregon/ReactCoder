69dd6f74f7639021cb50f98fb84e381a9cfc30ca
Andres Obregon <andresnicolasobregon@gmail.com>
2021-12-20 10:18:33 -0300

    className

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,22 +3,22 @@ import  React  from "react";
 const NavBar = () =>{
     return (
         <header>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="">Inicio</a>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="index.html">Inicio</a>
                         </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="">Carrito</a>
+                        <li className="nav-item">
+                        <a className="nav-link" href="">Carrito</a>
                         </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="">Quienes somos</a>
+                        <li className="nav-item">
+                        <a className="nav-link" href="">Quienes somos</a>
                         </li>
                     </ul>
                     </div>
