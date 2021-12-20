cec087acc99d231b717de5dea1161ea23ee81318
Andres Obregon <andresnicolasobregon@gmail.com>
2021-12-20 10:02:14 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import  React  from "react";
 import "./estilo.css"
-import NavBar from "./components/NavBar.js"
+
+import Header from "./components/Header";
 
 function App(){
     return (
-        <div>
-            <h1>E-commerce</h1>
-            <NavBar /> 
-        </div>
+        <Header /> 
     )     
 }
 export default App
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import  React  from "react";
+
+import NavBar from "./NavBar.js"
+
+function Header(){
+    return (
+        <header>
+            <h1>E-commerce</h1>
+            <NavBar /> 
+        </header>
+    )     
+}
+export default Header
