5fb8733e04aa751e5595a6f22d03f416975afefa
Andres Obregon <andresnicolasobregon@gmail.com>
2022-01-05 17:54:38 -0300

    filtrar por categoria

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -7,7 +7,7 @@ const ItemDetail  = ({item}) => {
         <img src={item.img} alt={item.nombre} width={100} ></img>
         <p>Nombre: { item.nombre}</p>
         <p>Precio: ${item.precio}</p>
-        <p>Detalle: ${item.detalle}</p>
+        <p>Detalle: {item.detalle}</p>
         <ItemCount stockProducto= {item.stock}  />
     </article>
     )
